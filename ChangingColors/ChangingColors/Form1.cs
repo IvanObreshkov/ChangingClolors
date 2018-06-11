7d457e63c4533a258e1f9faab0b13e09481d3b57
Ivan Obreshkov <ivanobreshkov12@gmail.com>
2018-06-11 11:27:48 +0300

    First and final commit

---
+++ ChangingColors/ChangingColors/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ChangingColors
+{
+    public partial class Form1 : Form
+    {
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            int a = 256, freq = 10;
+            Random c = new Random();
+            for (int i = 0; i <= 40; i++)
+            {
+                g.Clear(Color.FromArgb(c.Next() % a, c.Next() % a, c.Next() % a));
+                System.Threading.Thread.Sleep(1000);
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
